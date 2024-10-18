65f75859b0a02f56ffdceafc5d0245e741b5d4bc
jerrykankelborg <jkankelborg@csumb.edu>
2024-10-18 16:36:52 -0700

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const [pictures, setPictures] = useState([]);
+  const [weather, setWeather] = useState([]);
+
+  const fetchPics = async (weather) => {
+    try {
+      const response = await fetch(
+        `https://api.unsplash.com/search/photos/?query=${weather}&page=1&client_id=jtZrDriaHzgfjU77JCp_FSg0Xtqu65JUMeSTx49KiIQ`
+      );
+      const data = await response.json();
+      if (data) {
+        setPictures(data);
+      }
+    } catch (err) {
+      console.log("unsplash error jj");
+    }
+  };
+
+  let fetchAPI = async (locParam) => {
+    try {
+      const response = await fetch(
+        `https://api.openweathermap.org/data/2.5/weather?q=${locParam}&APPID=76d0bcbd868226d9268c69f256948555`
+      );
+      const data = await response.json();
+      if (data)
+        setWeather({
+          humidity: data.main.humidity,
+          windSpeed: data.wind.speed,
+          temperature: Math.floor(data.main.temp),
+          location: data.name,
+          country: data.sys.country,
+          visibility: data.visibility,
+          clouds: data.clouds.all,
+          weather: data.weather[0].main,
+        });
+      console.log(data);
+      fetchPics(data.weather[0].main);
+    } catch (err) {
+      console.log("oooooohh no!");
+    }
+  };
+  return <div className="">app.js</div>;
 }
 
 export default App;
