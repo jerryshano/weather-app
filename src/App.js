754cdee21db5c0295961303ce08f0e1cc32c798f
Clearview Carehome <clearview@Clearviews-iMac.local>
2024-10-20 15:20:04 -0700

    Build the layout

--- src/App.js
+++ src/App.js
@@ -1,48 +1,55 @@
-import { useState } from "react";
+// import { useState } from "react";
 import "./App.css";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import AppLayout from "./pages/AppLayout";
 
 function App() {
-  const [pictures, setPictures] = useState([]);
-  const [weather, setWeather] = useState([]);
+  // const [pictures, setPictures] = useState([]);
+  // const [weather, setWeather] = useState([]);
 
-  const fetchPics = async (weather) => {
-    try {
-      const response = await fetch(
-        `https://api.unsplash.com/search/photos/?query=${weather}&page=1&client_id=jtZrDriaHzgfjU77JCp_FSg0Xtqu65JUMeSTx49KiIQ`
-      );
-      const data = await response.json();
-      if (data) {
-        setPictures(data);
-      }
-    } catch (err) {
-      console.log("unsplash error jj");
-    }
-  };
+  // const fetchPics = async (weather) => {
+  //   try {
+  //     const response = await fetch(
+  //       `https://api.unsplash.com/search/photos/?query=${weather}&page=1&client_id=jtZrDriaHzgfjU77JCp_FSg0Xtqu65JUMeSTx49KiIQ`
+  //     );
+  //     const data = await response.json();
+  //     if (data) {
+  //       setPictures(data);
+  //     }
+  //   } catch (err) {
+  //     console.log("unsplash error jj");
+  //   }
+  // };
 
-  let fetchAPI = async (locParam) => {
-    try {
-      const response = await fetch(
-        `https://api.openweathermap.org/data/2.5/weather?q=${locParam}&APPID=76d0bcbd868226d9268c69f256948555`
-      );
-      const data = await response.json();
-      if (data)
-        setWeather({
-          humidity: data.main.humidity,
-          windSpeed: data.wind.speed,
-          temperature: Math.floor(data.main.temp),
-          location: data.name,
-          country: data.sys.country,
-          visibility: data.visibility,
-          clouds: data.clouds.all,
-          weather: data.weather[0].main,
-        });
-      console.log(data);
-      fetchPics(data.weather[0].main);
-    } catch (err) {
-      console.log("oooooohh no!");
-    }
-  };
-  return <div className="">app.js</div>;
+  // let fetchAPI = async (locParam) => {
+  //   try {
+  //     const response = await fetch(
+  //       `https://api.openweathermap.org/data/2.5/weather?q=${locParam}&APPID=76d0bcbd868226d9268c69f256948555`
+  //     );
+  //     const data = await response.json();
+  //     if (data)
+  //       setWeather({
+  //         humidity: data.main.humidity,
+  //         windSpeed: data.wind.speed,
+  //         temperature: Math.floor(data.main.temp),
+  //         location: data.name,
+  //         country: data.sys.country,
+  //         visibility: data.visibility,
+  //         clouds: data.clouds.all,
+  //         weather: data.weather[0].main,
+  //       });
+  //     console.log(data);
+  //   } catch (err) {
+  //     console.log("oooooohh no!");
+  //   }
+  // };
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route index element={<AppLayout />}></Route>
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
--- src/pages/AppLayout.jsx
+++ src/pages/AppLayout.jsx
@@ -1,5 +1,56 @@
+import styled from "styled-components";
+
+const FlexHeader = styled.div`
+  display: flex;
+  border: 2px solid darkgreen;
+  height: 100px;
+`;
+const FlexMain = styled.div`
+  display: flex;
+  min-height: calc(100vh - 100px);
+`;
+const FlexSideBar = styled.div`
+  border: 1px solid purple;
+  display: flex;
+  flex-grow: 1;
+`;
+const FlexContent = styled.div`
+  border: 1px solid darkblue;
+  display: flex;
+  flex-grow: 4;
+`;
+const FlexList = styled.div`
+  border: 1px solid darkblue;
+  display: flex;
+  flex-direction: column;
+  flex-grow: 2;
+`;
+const ListItem = styled.div`
+  border: 1px solid darkblue;
+  flex-grow: 1;
+`;
 function AppLayout() {
-  return <div>app layout</div>;
+  return (
+    <>
+      <FlexHeader>
+        <span>header</span>
+      </FlexHeader>
+      <FlexMain>
+        <FlexSideBar>
+          <span>sidebar</span>
+        </FlexSideBar>
+        <FlexContent>
+          <span>content area</span>
+        </FlexContent>
+        <FlexList>
+          <ListItem>list item</ListItem>
+          <ListItem>list item</ListItem>
+          <ListItem>list item</ListItem>
+          <ListItem>list item</ListItem>
+        </FlexList>
+      </FlexMain>
+    </>
+  );
 }
 
 export default AppLayout;
